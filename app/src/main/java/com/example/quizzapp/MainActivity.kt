6bf16e12fa961ea81dd10e1baded0398fa20c76e
Shubh-dev-19 <shubhkumarkesarwani@gmail.com>
2024-01-19 18:17:32 +0530

    Finally it got build.

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.quizzapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,17 +17,20 @@ class MainActivity : AppCompatActivity() {
         val BtnOrder = findViewById<Button>(R.id.btnOrder)
         BtnOrder.setOnClickListener {
             // Your code for handling button click goes here
+            val rgMeat=findViewById<RadioGroup>(R.id.rgMeat)
             val checkedMeatRadiobuttonId=rgMeat.checkedRadioButtonId
-            val meat=findViewById<Button>(R.id.checkedMeatRadiobuttonId)
-            val cheese=cbCheese.ischecked
-            val onions=cbOnion.ischecked
-            val salads=cbSalad.ischecked
-             val orderString="You ordered a burgr with:\n"+
-                     "${meat.text}"+
-                     (if(cheese) "\nCheese" else "")+
-                     (if(onions) "\nonions" else "")+
-                     (if(salads) "\nSalad" else "")
+            val meat=findViewById<RadioButton>(checkedMeatRadiobuttonId)
+            val cheese=findViewById<CheckBox>(R.id.cbCheese).isChecked
+            val onions=findViewById<CheckBox>(R.id.cbOnion).isChecked
+            val salads=findViewById<CheckBox>(R.id.cbSalad).isChecked
+            val tvOrder=findViewById<TextView>(R.id.tvOrder)
+            val orderString="You ordered a burgr with:\n"+
+                    "${meat.text}"+
+                    (if(cheese) "\nCheese" else "")+
+                    (if(onions) "\nonions" else "")+
+                    (if(salads) "\nSalad" else "")
             tvOrder.text=orderString
         }
     }
 }
+
